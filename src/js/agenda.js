4b1cf92f682b80b3e1a9f222a5a4c9dcfab70970
Denisse Rossel <denisserosseln@gmail.com>
2024-12-05 20:47:48 -0300

    Subiendo búsqueda por especialidad en contactos

--- src/js/agenda.js
+++ src/js/agenda.js
@@ -1,5 +1,6 @@
 import '../scss/styles.scss'
 import { AppointmentStack, createAppointmentListItemHTML } from './components/appointments';
+import { fetchDoctorData } from './api/doctors';
 
 const appointmentStack = new AppointmentStack(renderAppointmentList);
 
