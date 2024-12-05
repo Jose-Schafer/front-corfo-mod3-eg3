1a324f434606a59230e16ecd4ca75782b5185094
Pablo-Soto-Falcon <pablosf10@gmail.com>
2024-12-04 21:22:03 -0300

    Agregar precio a las subclases de doctores

--- src/js/components/doctors/specialist.js
+++ src/js/components/doctors/specialist.js
@@ -14,50 +14,66 @@ class Doctor {
 
 class traumatología extends Doctor {
     constructor(name, image, experience, diplomas) {
-        super(name, "Traumatólogo", image, experience, diplomas);
+        super(name, "especialista en traumatología.", image, experience, diplomas);
     }
 
     performSurgery() {
         return `${this._name} se desempeña en cirugía traumatológica.`;
     }
+    price() {
+        return $20000;
+    }
 }
 
 class otorrinolaringología extends Doctor {
     constructor(name, image, experience, diplomas) {
-        super(name, "Otorrinolaringólogo", image, experience, diplomas);
+        super(name, "Oespecialista en otorrinolaringología.", image, experience, diplomas);
     }
 
     treatEarInfection() {
         return `${this._name} trata la infección de oído.`;
     }
+    price() {
+        return $21000;
+    }
 }
 
 class kinesiología extends Doctor {
     constructor(name, image, experience, diplomas) {
-        super(name, "Kinesiólogo", image, experience, diplomas);
+        super(name, "especialista en kinesiología.", image, experience, diplomas);
     }
 
     treatEarInfection() {
         return `${this._name} ayuda a mejorar la movilidad del musculo esquelético.`;
     }
+    price() {
+        return $15000;
+    }
 }
 
 class gediatría extends Doctor {
     constructor(name, image, experience, diplomas) {
-        super(name, "Gediatra", image, experience, diplomas);
+        super(name, "especialista en gediatría.", image, experience, diplomas);
     }
 
     treatEarInfection() {
         return `${this._name} encargado de la salud del adulto mayor.`;
     }
+    price() {
+        return $25000;
+    }
 }
 
 class general extends Doctor {
     constructor(name, image, experience, diplomas) {
-        super(name, "Medico General", image, experience, diplomas);
+        super(name, "Médico General", image, experience, diplomas);
     }
 
     treatEarInfection() {
         return `${this._name} puede atender cualquier tipo de enfermedad y derivar a un especialista.`;
     }
+    price() {
+        return $10000;
+    }
 }
+
